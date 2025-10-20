d63a13f70ecde8766efa5edbd31b10e0fae40b1e
Rodrigo Espinosa <rodesar95@hotmail.com>
2025-10-20 17:27:20 -0600

    UPDATE: use default list name for index view

--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,6 @@ from django.views import generic
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
 
     def get_queryset(self):
         return Question.objects.order_by("-pub_date")[:5]
