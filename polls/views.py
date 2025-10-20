4a83ac0316962a62e3c4dd36d314e0b9df58548f
Rodrigo Espinosa <rodesar95@hotmail.com>
2025-10-20 17:14:59 -0600

    UPDATE: use generic views to simplify redundant code

--- polls/views.py
+++ polls/views.py
@@ -1,32 +1,29 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render
+from django.http import  Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import F
 from .models import Question, Choice
+from django.views import generic
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-    return render(request, "polls/index.html", context)
-
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    
-    context = {"question": question}
-    return render(request, "polls/detail.html", context)
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-    return render(request, "polls/results.html", {"question": question})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     try:
